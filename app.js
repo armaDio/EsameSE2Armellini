35a187a8a65031940ef04a08bb3c0841354235c8
Michele Armellini <armellinimichele@live.it>
2018-01-26 11:40:27 +0100

    Merged checker (#1)
    * add function compareResults
    
    * add empty function check
    
    * Implementata funzione check in checker.js
    
    * added post handler in app.js
    
    * Added procfile for Heroku
    
    * added dependencies
    
    * added another dependency
    
    * solved small bug
    
    * solved another small bug
    
    * resolved major issue
    
    * Finally solved another small bug

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express'),
     bodyParser = require('body-parser');
-
+const checker = require('./checker');
 const app = express();
 app.use(bodyParser.json());
 
@@ -14,6 +14,16 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 3})
+})
+
+app.post('/check',function (req, res) {
+    var parameters = req.body;
+    new Promise(function(resolve,reject){
+
+      resolve(checker(parameters.url, parameters.invocationParameters, parameters.expectedResultData, parameters.expectedResultStatus));
+    }).then((resolve,err) => {
+      res.send(resolve);});
+
 })
 
 app.listen(app.get('port'), function() {
---
+++ checker.js
@@ -0,0 +1,37 @@
+
+var request = require('request');
+
+function check(url, invocationParameters,  expectedResultData, expectedResultStatus) {
+
+    const checkResult = { // this is the object you need to set and return
+        urlChecked: url,
+        resultData: null,
+        resultStatus: null,
+        statusTestPassed: null,
+        resultDataAsExpected: null
+    }
+    value = {};
+    return new Promise(function(resolve,reject){
+          request.get({"url": url, "qs": invocationParameters}, function(err, res, body) {
+    			   checkResult.resultStatus = res.statusCode;
+             checkResult.statusTestPassed = (res.statusCode == expectedResultStatus);
+             checkResult.resultData = JSON.parse(res.body);
+             checkResult.resultDataAsExpected = compareResults(expectedResultData, JSON.parse(res.body));
+             resolve(checkResult);
+		      });
+    });
+}
+
+
+// funzione che confronta due oggetti semplici e verifica se actual contiene tutti gli attributi di expected, e se per
+// questi ha gli stessi valori
+function compareResults(expected, actual) {
+    if (!expected) return true //always ok if there are no expectations
+    if (!actual) return false
+    for (let e of Object.keys(expected)) {
+        if (actual[e]===undefined || expected[e]!=actual[e]  ) return false
+    }
+    return true
+}
+
+module.exports = check
